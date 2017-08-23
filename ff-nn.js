10201b52d7da9988172600ae094237016e93440f
Tapani <tapani.e.toivonen@gmail.com>
2017-08-23 11:26:28 +0300

    Update ff-nn.js

--- ff-nn.js
+++ ff-nn.js
@@ -45,10 +45,10 @@ NN.FeedForwardNN = function(args) {
 	this.activationInputs = [],
 	this.activationHiddens = [],
 	this.activationOutputs = [],
-	this.weightInputs 	= [],
-	this.weightOutputs 	= [],
-	this.changeInputs 	= [], 
-	this.changeOutputs 	= [];
+	this.weightInputs = [],
+	this.weightOutputs = [],
+	this.changeInputs = [], 
+	this.changeOutputs = [];
 
 	this.__initNet();
 
